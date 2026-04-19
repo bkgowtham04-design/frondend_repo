49e90bde0911e2ce5ff1bf5c5646de2c5ae1039d
bkgowtham04-design <bkgowtham04@gmail.com>
2026-04-19 19:55:31 +0530

    DONE

--- react/day3/website/src/App.jsx
+++ react/day3/website/src/App.jsx
@@ -5,12 +5,15 @@ import Navbar from "./components/Navbar"
 
 
 import './App.css'
+import Bannere from "./components/Bannere"
 
 const App =() =>{
 
 return(<>
 
 <Navbar/>
+
+<Bannere/>
 
 
 
---
+++ react/day3/website/src/components/Bannere.jsx
@@ -0,0 +1,15 @@
+import image from '../assets/1.jpg'
+
+const Bannere = () => {
+  return (
+    <>
+    
+    <div className="virat">
+        <img className="banner-img" src={image} alt="" />
+    </div>
+    
+    </>
+  )
+}
+
+export default Bannere
